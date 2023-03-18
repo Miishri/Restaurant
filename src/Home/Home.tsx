af0acdf9f32607a152bbb68280bebab5e6b2cb67
school laptop <muhammad.shamaeem@elev.ga.ntig.se>
2023-03-18 16:24:19 +0100

    fix website design

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,8 +1,9 @@
-import "./Home.css";
+import Navbar from "./Navbar";
 
 function Home() {
     return (
         <>
+        <Navbar/>
         </>
     );
 }
---
+++ src/Home/Navbar.tsx
@@ -0,0 +1,22 @@
+import "./Home.css"
+
+function Navbar() {
+
+    
+
+    return (
+        <div className="Navbar">
+            <button className="Menu option">
+                Menu
+            </button>
+            <button className="Recipe option">
+                Recipe
+            </button>
+            <button className="About option">
+                About
+            </button>
+        </div>  
+    );
+}
+
+export default Navbar;
